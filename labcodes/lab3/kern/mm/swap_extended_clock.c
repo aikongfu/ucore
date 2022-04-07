66dc096a1085df8647cb0a0b906d5a98bcfea11d
mingshashan <mingshashan@gmail.com>
2022-04-07 11:54:35 +0800

    lab3 challenge1

--- labcodes/lab3/kern/mm/swap_extended_clock.c
+++ labcodes/lab3/kern/mm/swap_extended_clock.c
@@ -100,7 +100,7 @@ _extended_clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, i
 
 static int
 _extended_clock_check_swap(void) {
-   cprintf("write Virt Page c in fifo_check_swap\n");
+    cprintf("write Virt Page c in fifo_check_swap\n");
     *(unsigned char *)0x3000 = 0x0c;
     assert(pgfault_num==4);
     cprintf("write Virt Page a in fifo_check_swap\n");
@@ -123,20 +123,39 @@ _extended_clock_check_swap(void) {
     assert(pgfault_num==6);
     cprintf("write Virt Page b in fifo_check_swap\n");
     *(unsigned char *)0x2000 = 0x0b;
-    assert(pgfault_num==7);
+    assert(pgfault_num==6);
     cprintf("write Virt Page c in fifo_check_swap\n");
     *(unsigned char *)0x3000 = 0x0c;
-    assert(pgfault_num==8);
+    assert(pgfault_num==7);
     cprintf("write Virt Page d in fifo_check_swap\n");
     *(unsigned char *)0x4000 = 0x0d;
-    assert(pgfault_num==9);
+    assert(pgfault_num==8);
     cprintf("write Virt Page e in fifo_check_swap\n");
     *(unsigned char *)0x5000 = 0x0e;
-    assert(pgfault_num==10);
+    assert(pgfault_num==9);
     cprintf("write Virt Page a in fifo_check_swap\n");
     assert(*(unsigned char *)0x1000 == 0x0a);
     *(unsigned char *)0x1000 = 0x0a;
+    assert(pgfault_num==9);
+    cprintf("read Virt Page b in fifo_check_swap\n");
+    assert(*(unsigned char *)0x2000 == 0x0b);
+    assert(pgfault_num==10);
+    cprintf("read Virt Page c in fifo_check_swap\n");
+    assert(*(unsigned char *)0x3000 == 0x0c);
     assert(pgfault_num==11);
+    cprintf("read Virt Page a in fifo_check_swap\n");
+    assert(*(unsigned char *)0x1000 == 0x0a);
+    assert(pgfault_num==12);
+    cprintf("read Virt Page d in fifo_check_swap\n");
+    assert(*(unsigned char *)0x4000 == 0x0d);
+    assert(pgfault_num==13);
+    cprintf("read Virt Page b in fifo_check_swap\n");
+    *(unsigned char *)0x1000 = 0x0a;
+    assert(*(unsigned char *)0x3000 == 0x0c);
+    assert(*(unsigned char *)0x4000 == 0x0d);
+    assert(*(unsigned char *)0x5000 == 0x0e);
+    assert(*(unsigned char *)0x2000 == 0x0b);
+    assert(pgfault_num==14);
     return 0;
 }
 
